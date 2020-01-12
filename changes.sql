5e8e081f9cd8e75e32af6d7e973bc21c7e95b1f5
VKS PC <vks.ecommerce@gmail.com>
2020-01-12 06:47:44 +0300

    first push

---
+++ changes.sql
@@ -0,0 +1,44 @@
+INSERT INTO `site_pages` (`id`, `parent`, `module_id`, `name`, `url`, `redirect_url`, `alias`, `header`, `title`,
+ `keywords`, `description`, `text`, `layout`, `works_type`, `active`, `sitemap`, `time_create`, `time_update`, `sort`)
+  VALUES (NULL, '0', '0', 'Туры по Петербургу', '/tours/', '', '', 'Туры по Санкт-Петербургу', '', '', '', '', '1', '0',
+   '1', '0', '0000-00-00 00:00:00', '2018-08-20 19:32:40', '0');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
